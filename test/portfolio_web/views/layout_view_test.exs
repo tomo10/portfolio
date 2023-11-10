e5fcd55ae0ca662c4c0857849573936e1e402f0d
tomo <thce28@gmail.com>
2023-11-10 11:01:37 +0000

    Tests passing removed trace tests of db

--- test/portfolio_web/views/layout_view_test.exs
+++ test/portfolio_web/views/layout_view_test.exs
@@ -1,5 +1,5 @@
 defmodule PortfolioWeb.LayoutViewTest do
-  use PortfolioWeb.ConnCase, async: true
+  # use PortfolioWeb.ConnCase, async: true
 
   # When testing helpers, you may want to import Phoenix.HTML and
   # use functions such as safe_to_string() to convert the helper
