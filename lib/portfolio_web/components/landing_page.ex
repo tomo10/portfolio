406ce39a25bd7dacee3e65bec01b962d59298038
tomo <thce28@gmail.com>
2023-11-10 10:50:21 +0000

    All databse postgres removed. Redeploy successful

--- lib/portfolio_web/components/landing_page.ex
+++ lib/portfolio_web/components/landing_page.ex
@@ -167,9 +167,6 @@ defmodule PortfolioWeb.Components.LandingPage do
               <div class="flex-1 p-4 text-left hover:bg-gray-800">
                 <h3 class="text-xl font-semibold mb-2"><%= blog.title %></h3>
                 <p class="text-sm text-gray-700 mb-4"><%= blog.description %></p>
-                <%!-- <button class="bg-gray-200 text-gray-700 py-2 px-4 rounded hover:bg-gray-300 focus:outline-none">
-                  Read
-                </button> --%>
                 <.button color="secondary" label="Read" variant="inverted" />
               </div>
             </div>
